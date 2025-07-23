531a04a31d5bdfe828c3b5b49f5c5e1c08d06c6e
Dade1991 <dade91@msn.com>
2025-07-23 15:46:28 +0200

    Some changes

--- u3-w1-d3-e-book-v2/src/components/BookList.jsx
+++ u3-w1-d3-e-book-v2/src/components/BookList.jsx
@@ -1,17 +1,22 @@
 import SingleBook from "./SingleBook"
+import { Container, Row } from "react-bootstrap"
 
 const BookList = (props) => {
   return (
     <>
       {props.bookArray.map((book) => {
         return (
-          <SingleBook
-            asin={book.asin}
-            title={book.title}
-            imgCover={book.img}
-            price={book.price}
-            category={book.category}
-          />
+          <Container>
+            <Row className="justify-content-center mt-5">
+              <SingleBook
+                asin={book.asin}
+                title={book.title}
+                imgCover={book.img}
+                price={book.price}
+                category={book.category}
+              />
+            </Row>
+          </Container>
         )
       })}
     </>
